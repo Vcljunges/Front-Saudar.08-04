3c6b9245933533feefa926a6be4862290a31539c
Vinícius <vcljunges@gmail.com>
2026-04-08 15:59:06 -0300

    Criação das horas em tempo real

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Home() {
+  const hora = new Date().getHours();
+  const minuto = new Date().getMinutes();
+  const horaFormatada = `${hora}:${minuto}`;
+
+  const saudacao = () => {
+    if (hora >= 5 && hora < 12) {
+      return "Bom dia";
+    } else if (hora >= 12 && hora < 18) {
+      return "Boa tarde";
+    } else {
+      return "Boa noite";
+    }
+  };
+
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-blue-500">
+      <h1 className="text-9xl font-bold">{horaFormatada}</h1>
+      <h1 className="text-5xl font-bold">{saudacao()}</h1>
+    </div>
+  );
+}
